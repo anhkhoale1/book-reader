9662aa46691dd8a334449f3e2a670e0822c76854
anhkhoale1 <leanhkhoa199937@gmail.com>
2023-07-06 10:34:29 +0200

    add button play and pause

--- src/components/BookReader.tsx
+++ src/components/BookReader.tsx
@@ -1,5 +1,10 @@
-import { Typography } from '@mui/material';
-import { useState, useEffect } from 'react';
+import { Box, ButtonGroup, Card, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import IconButton from "@mui/material/IconButton";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PauseIcon from "@mui/icons-material/Pause";
 
 interface BookProps {
   book: string[];
@@ -7,26 +12,78 @@ interface BookProps {
 
 const BookReader = (props: BookProps) => {
   const { book } = props;
-  const [currentWord, setCurrentWord] = useState('');
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentWord, setCurrentWord] = useState(book[0]);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const play = (): void => {
+    setIsPlaying(true);
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    let intervalId: NodeJS.Timeout | null = null;
+
+    const updateCurrentWord = (): void => {
       if (currentIndex < book.length) {
         setCurrentWord(book[currentIndex]);
-        setCurrentIndex(prevIndex => prevIndex + 1);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        clearInterval(intervalId!);
+        intervalId = null;
       }
-    }, 200);
+    };
+
+    if (isPlaying) {
+      intervalId = setInterval(updateCurrentWord, 200);
+    }
 
     return () => {
-      clearInterval(interval);
+      clearInterval(intervalId!);
     };
-  }, [currentIndex, book]);
+  }, [isPlaying, currentIndex, book]);
+
+  const pause = (): void => {
+    setIsPlaying(false);
+  };
 
   return (
-    <>
-      <Typography>{currentWord}</Typography>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        gap: "1rem",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        sx={{
+          width: "200px",
+          height: "200px",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <Typography>{currentWord}</Typography>
+      </Card>
+      <ButtonGroup sx={{ display: "flex", justifyContent: "center" }}>
+        <IconButton>
+          <SkipPreviousIcon />
+        </IconButton>
+        {isPlaying ? (
+          <IconButton onClick={pause}>
+            <PauseIcon />
+          </IconButton>
+        ) : (
+          <IconButton onClick={play}>
+            <PlayArrowIcon />
+          </IconButton>
+        )}
+        <IconButton>
+          <SkipNextIcon />
+        </IconButton>
+      </ButtonGroup>
+    </Box>
   );
 };
 
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,12 +1,17 @@
-import { Container } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import BookReader from './BookReader';
 
 const Page = () => {
-  const book = ['This', 'is', 'a', 'sample', 'book'];
+  const paragraph = "Năm 1998, ông được Nhà xuất bản Kim Đồng trao giải Nhà văn có sách bán chạy nhất. Năm 2003, bộ truyện nhiều tập Kính vạn hoa được Trung ương Đoàn Thanh niên Cộng sản Hồ Chí Minh trao huy chương Vì thế hệ trẻ và được Hội Nhà Văn Việt Nam trao giải thưởng. Đến nay ông đã xuất bản gần 100 tác phẩm và từ lâu đã trở thành nhà văn thân thiết của các bạn đọc nhỏ tuổi ở Việt Nam."
+  const book = paragraph.split(" ");
+  console.log(book);
 
   return (
     <Container>
-      <BookReader book={book} />
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '1rem' }}>
+        <Typography>Tên chương</Typography>
+        <BookReader book={book} />
+      </Box>
     </Container>
   );
 };
